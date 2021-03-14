0f43ebc814fffa6e25ff8ecdedcbccc605edb710
lior sanders <sanderslior@gmail.com>
2021-03-14 18:33:14 +0000

    Deleted Gallery/Album.cpp, Gallery/Album.h, Gallery/AlbumManager.cpp, Gallery/AlbumManager.h, Gallery/AlbumNotOpenException.h, Gallery/Constants.h, Gallery/Gallery.cpp, Gallery/Gallery.sln, Gallery/Gallery.vcxproj, Gallery/Gallery.vcxproj.filters, Gallery/IDataAccess.h, Gallery/ItemNotFoundException.h, Gallery/MemoryAccess.cpp, Gallery/MemoryAccess.h, Gallery/MyException.h, Gallery/Picture.cpp, Gallery/Picture.h, Gallery/ReadMe.txt, Gallery/User.cpp, Gallery/User.h files

--- Gallery/AlbumNotOpenException.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "MyException.h"
-
-class AlbumNotOpenException : public MyException {
-public:
-	AlbumNotOpenException(): MyException("Error: Can't perform operation, you need to open an album first") {}
-};
